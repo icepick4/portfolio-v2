8e47a7a482b76d794749ab8fde8a2cf7cbf63093
Icepick <remi.jara@laposte.net>
2024-07-02 15:35:10 +0200

    Initiate the repo with Nuxt & NuxtUI & Resend

---
+++ layouts/page.vue
@@ -0,0 +1,7 @@
+<template>
+    <div
+        class="w-full h-full flex flex-col justify-center items-center dark:bg-neutral-900"
+    >
+        <slot />
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+    <UButton>Click me</UButton>
+</template>

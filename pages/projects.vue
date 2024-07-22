ad3ff3a3e690a3c829e1e1427e0c8df1cf0106f8
Rémi JARA <remi.jara@laposte.net>
2024-07-22 19:34:09 +0200

    Reading JSON files with Nuxt Content

---
+++ pages/projects.vue
@@ -0,0 +1,19 @@
+<script setup>
+const { data } = await useAsyncData(() => queryContent('projects').find());
+
+const projects = data.value[0].body;
+</script>
+
+<template>
+    <p>Projects:</p>
+    <div v-for="project in projects" :key="project.slug">
+        <h2>{{ project.name }}</h2>
+        <p>{{ project.description }}</p>
+        <p>{{ project.status }}</p>
+        <p>{{ project.date }}</p>
+        <p>{{ project.type }}</p>
+        <p>{{ project.technos }}</p>
+        <p>{{ project.link }}</p>
+        <p>{{ project.repo_link }}</p>
+    </div>
+</template>

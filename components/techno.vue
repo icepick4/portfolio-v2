a954d9a7a78d7f3a7420958144a8548829007606
Icepick <remi.jara@laposte.net>
2024-08-20 19:55:33 +0200

    Adding all tools on projects + assing tooltip hover on them

--- components/techno.vue
+++ components/techno.vue
@@ -5,7 +5,7 @@ const props = defineProps<{
     size: 'little' | 'medium' | 'big';
 }>();
 
-const techno = ref(findTechnoByTitle(props.techno));
+const technoObject = ref(findTechnoByTitle(props.techno));
 
 const colorMode = useColorMode();
 
@@ -17,54 +17,89 @@ const bigClassesDisplay =
 <template>
     <ClientOnly>
         <div
-            v-if="techno"
+            v-if="technoObject"
             :class="{
                 [littleClassesDisplay]: size === 'little',
                 [bigClassesDisplay]: size === 'big'
             }"
         >
             <p v-if="size == 'little'" class="mr-2 text-xs">
-                • {{ techno.title }}
+                • {{ technoObject.title }}
             </p>
             <p
                 v-else-if="size == 'big'"
                 class="text-xs font-bold text-stone-800 dark:text-neutral-400"
             >
-                {{ techno.title }}
+                {{ technoObject.title }}
             </p>
-            <i
-                v-if="techno.icon"
-                :class="[
-                    `devicon-${techno.icon}-original devicon-${techno.icon}-plain`,
-                    {
-                        'text-xl': size == 'little',
-                        'text-3xl': size == 'medium',
-                        'text-5xl': size === 'big'
-                    },
-                    {
-                        colored:
-                            (techno.icon !== 'github' &&
-                                techno.icon !== 'symfony' &&
-                                techno.icon !== 'threejs' &&
-                                techno.icon !== 'oauth' &&
-                                techno.icon !== 'linux' &&
-                                techno.icon !== 'vercel' &&
-                                techno.icon !== 'markdown' &&
-                                colorMode.value === 'dark') ||
-                            colorMode.value === 'light'
-                    }
-                ]"
-            ></i>
-            <img
-                v-if="!techno.icon"
-                :src="`logos/${techno.image}.webp`"
-                alt="logo"
-                :class="{
-                    'w-4 h-4': size == 'little',
-                    'w-8 h-8': size == 'medium',
-                    'w-12 h-12': size === 'big'
-                }"
-            />
+            <div class="group/label relative">
+                <i
+                    v-if="technoObject.icon"
+                    :class="[
+                        `devicon-${technoObject.icon}-original devicon-${technoObject.icon}-plain`,
+                        {
+                            'text-xl': size == 'little',
+                            'text-3xl': size == 'medium',
+                            'text-5xl': size === 'big'
+                        },
+                        {
+                            colored:
+                                (technoObject.icon !== 'github' &&
+                                    technoObject.icon !== 'symfony' &&
+                                    technoObject.icon !== 'threejs' &&
+                                    technoObject.icon !== 'oauth' &&
+                                    technoObject.icon !== 'linux' &&
+                                    technoObject.icon !== 'vercel' &&
+                                    technoObject.icon !== 'markdown' &&
+                                    colorMode.value === 'dark') ||
+                                colorMode.value === 'light'
+                        }
+                    ]"
+                ></i>
+                <UBadge
+                    class="text-xs group-hover/label:opacity-100 transition-all opacity-0 absolute -top-4 -right-10 z-10"
+                    color="gray"
+                    >{{ techno }}</UBadge
+                >
+            </div>
+            <div class="group/label relative">
+                <img
+                    v-if="!technoObject.icon"
+                    :src="`logos/${technoObject.image}.webp`"
+                    alt="logo"
+                    :class="{
+                        'w-4 h-4': size == 'little',
+                        'w-8 h-8': size == 'medium',
+                        'w-12 h-12': size === 'big'
+                    }"
+                />
+                <UBadge
+                    class="text-xs group-hover/label:opacity-100 transition-all opacity-0 absolute -top-4 -right-10 z-10"
+                    color="gray"
+                    >{{ techno }}</UBadge
+                >
+            </div>
+        </div>
+        <div v-else>
+            <div class="group/label relative">
+                <img
+                    :src="`logos/${techno
+                        .toLocaleLowerCase()
+                        .replace(' ', '-')
+                        .replace('.', '')}.webp`"
+                    alt="logo"
+                    :class="{
+                        'w-4 h-4': size == 'little',
+                        'w-8 h-8': size == 'medium',
+                        'w-12 h-12': size == 'big'
+                    }"
+                />
+                <UBadge
+                    class="text-xs group-hover/label:opacity-100 transition-all opacity-0 absolute -top-4 -right-10 z-10"
+                    color="gray"
+                    >{{ techno }}</UBadge
+                >
+            </div>
         </div>
     </ClientOnly>
 </template>
